721a68e3dd7222e675f8d9bcaa5ebe02f712f0ac
spac3orange <m4gi3k44@proton.me>
2024-01-12 05:41:35 +0600

    upd 4

--- main.py
+++ main.py
@@ -15,16 +15,27 @@ async def start_params() -> None:
 
 async def send_btc_rate(target_channel):
     url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,rub"
+    previous_btc_usd = 0
     while True:
         response = requests.get(url)
         data = response.json()
         btc_usd = data["bitcoin"]["usd"]
         btc_rub = data["bitcoin"]["rub"]
-        btc_usd = '{:,.2f}'.format(btc_usd).replace(',', ' ')
-        btc_rub = '{:,.2f}'.format(btc_rub).replace(',', ' ')
+        btc_usd = '{:,.2f}'.format(btc_usd).replace(',', '')
+        btc_rub = '{:,.2f}'.format(btc_rub).replace(',', '')
+        btc_usd, btc_rub = btc_usd[:6].rstrip('.'), btc_rub[:8].rstrip('.')
+        print(btc_usd, btc_rub)
 
-        msg = f'💰 <b>1 BTC</b> | 🇺🇸 <b>${btc_usd[:6]}</b> | 🇷🇺 <b>₽{btc_rub[:9]}</b>'
+        if previous_btc_usd:
+            percent_change_usd = ((int(btc_usd) - int(previous_btc_usd)) / int(previous_btc_usd)) * 100
+
+            percent_change_usd_str = f"🔺{abs(percent_change_usd):.2f}%" if percent_change_usd > 0 else f"🔻{abs(percent_change_usd):.2f}%"
+            msg = f'💰 <b>1 BTC</b> | 🇺🇸 <b>${btc_usd[:6]}</b> | 🇷🇺 <b>₽{btc_rub[:9]}</b> | {percent_change_usd_str}'
+        else:
+            msg = f'💰 <b>1 BTC</b> | 🇺🇸 <b>${btc_usd[:6]}</b> | 🇷🇺 <b>₽{btc_rub[:9]}</b>'
+
         await aiogram_bot.send_message(target_channel, msg, parse_mode='HTML')
+        previous_btc_usd = btc_usd
         await asyncio.sleep(3600)
 
 async def main():
