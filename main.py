2c3a75feccde8cf33fe27b3edef3dc63ffdf5d13
spac3orange <m4gi3k44@proton.me>
2024-05-31 15:50:50 +0300

    upd 5.2

--- main.py
+++ main.py
@@ -12,33 +12,43 @@ async def start_params() -> None:
     await aiogram_bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(aiogram_bot, allowed_updates=["message", "inline_query", "chat_member", "chat_join_request", "callback_query"])
 
+async def try_get_rate():
+    previous_btc_usd = 0
+    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,rub"
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+    btc_usd = data["bitcoin"]["usd"]
+    btc_rub = data["bitcoin"]["rub"]
+    btc_usd = '{:,.2f}'.format(btc_usd).replace(',', ' ')
+    btc_rub = '{:,.2f}'.format(btc_rub).replace(',', ' ')
+    btc_usd, btc_rub = btc_usd[:6].rstrip('.'), btc_rub[:9].rstrip('.')
+    print(btc_usd, btc_rub)
+    print(previous_btc_usd)
+
+    if previous_btc_usd:
+        percent_change_usd = ((int(btc_usd.replace(' ', '')) - previous_btc_usd) / previous_btc_usd) * 100
+
+        percent_change_usd_str = f"🔺{abs(percent_change_usd):.2f}%" if percent_change_usd > 0 else f"🔻{abs(percent_change_usd):.2f}%"
+        msg = f'💰 <b>1 BTC</b> | 🇺🇸 <b>${btc_usd[:6]}</b> | 🇷🇺 <b>₽{btc_rub[:9]}</b> | {percent_change_usd_str}'
+    else:
+        msg = f'💰 <b>1 BTC</b> | 🇺🇸 <b>${btc_usd[:6]}</b> | 🇷🇺 <b>₽{btc_rub[:9]}</b>'
+
+    await aiogram_bot.send_message(target_channel, msg, parse_mode='HTML')
+    print('rate sent')
+    previous_btc_usd = int(btc_usd.replace(' ', ''))
+    await asyncio.sleep(3600)
+
 
 async def send_btc_rate(target_channel):
-    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd,rub"
-    previous_btc_usd = 0
+
     while True:
-        response = requests.get(url)
-        data = response.json()
-        btc_usd = data["bitcoin"]["usd"]
-        btc_rub = data["bitcoin"]["rub"]
-        btc_usd = '{:,.2f}'.format(btc_usd).replace(',', ' ')
-        btc_rub = '{:,.2f}'.format(btc_rub).replace(',', ' ')
-        btc_usd, btc_rub = btc_usd[:6].rstrip('.'), btc_rub[:9].rstrip('.')
-        print(btc_usd, btc_rub)
-        print(previous_btc_usd)
+        try:
+            await asyncio.wait_for(try_get_rate(), timeout=15)
+        except asyncio.TimeoutError as e:
+            print(f'error: {e}')
+            continue
 
-        if previous_btc_usd:
-            percent_change_usd = ((int(btc_usd.replace(' ', '')) - previous_btc_usd) / previous_btc_usd) * 100
-
-            percent_change_usd_str = f"🔺{abs(percent_change_usd):.2f}%" if percent_change_usd > 0 else f"🔻{abs(percent_change_usd):.2f}%"
-            msg = f'💰 <b>1 BTC</b> | 🇺🇸 <b>${btc_usd[:6]}</b> | 🇷🇺 <b>₽{btc_rub[:9]}</b> | {percent_change_usd_str}'
-        else:
-            msg = f'💰 <b>1 BTC</b> | 🇺🇸 <b>${btc_usd[:6]}</b> | 🇷🇺 <b>₽{btc_rub[:9]}</b>'
-
-        await aiogram_bot.send_message(target_channel, msg, parse_mode='HTML')
-        print('rate sent')
-        previous_btc_usd = int(btc_usd.replace(' ', ''))
-        await asyncio.sleep(3600)
 
 async def main():
     task1 = asyncio.create_task(start_params())
